53c13da9cc579132dfde1ea8a29f52cd00dec4be
Tommy <tmackcsb@gmail.com>
2026-04-10 19:16:10 +0100

    src: basic deck stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::prelude::*;
+// use std::io;
+
+#[derive(Copy, Clone, Debug)]
 pub enum Suit {
     Spades,
     Clubs,
@@ -5,6 +9,13 @@ pub enum Suit {
     Hearts,
 }
 
+impl Suit {
+    pub fn suits() -> Vec<Suit> {
+        vec![Suit::Spades, Suit::Clubs, Suit::Diamonds, Suit::Hearts]
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 pub enum Val {
     One,
     Two,
@@ -18,15 +29,72 @@ pub enum Val {
     King,
 }
 
-pub enum Card {
-    Suit(Suit),
-    Val(Val),
+impl Val {
+    pub fn vals() -> Vec<Val> {
+        vec![
+            Val::One,
+            Val::Two,
+            Val::Three,
+            Val::Four,
+            Val::Five,
+            Val::Six,
+            Val::Seven,
+            Val::Jack,
+            Val::Queen,
+            Val::King,
+        ]
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Card {
+    suit: Suit,
+    val: Val,
+}
+
+impl Card {
+    pub fn new(suit: Suit, val: Val) -> Card {
+        Card { suit, val }
+    }
 }
 
 pub struct Player {
-    Hand: Vec<Card>,
+    hand: Vec<Card>,
+    pond: Vec<Card>,
+}
+
+impl Player {
+    pub fn split_the_deck(cards: Vec<Card>) {
+        todo!();
+    }
+}
+
+pub struct Game {
+    players: Vec<Player>,
+}
+
+fn new_deck() -> Vec<Card> {
+    let vals = Val::vals();
+    let suits = Suit::suits();
+    let mut deck = vec![];
+    for v in vals {
+        for s in &suits {
+            deck.push(Card::new(*s, v));
+        }
+    }
+    deck
 }
 
 fn main() {
+    let mut rng = rand::rng();
+    // create a deck
+    let mut deck = new_deck();
+    deck.shuffle(&mut rng);
+
+    for i in 0..deck.len() {
+        print!("{:?}, ", deck[i]);
+    }
+    println!();
+
     println!("Hello, world!");
 }
