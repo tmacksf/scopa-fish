7a5dd449de7019f1158147b4dca0ab1da568518a
Thomas <thomas.mack@motorolasolutions.com>
2026-04-10 18:22:42 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+pub enum Suit {
+    S,
+    C,
+    D,
+    B,
+}
+
+pub enum Val {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    J,
+    Q,
+    K,
+}
+
+pub enum Card {
+    Suit(Suit),
+    Val(Val),
+}
+
+pub struct Player {
+    Hand: Vec<Card>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
